29220fd989ccebcfbfb154a71d9b5348441df223
MIkhail <mmv-774@yandex.ru>
2021-11-09 19:13:58 +0500

    feat: реализовать добавление карточек с помощью JS

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,44 @@ const closeBtn = popupElement.querySelector('.popup__btn');
 const formElement = popupElement.querySelector('.form');
 const nameInput = formElement.querySelector('#user-name');
 const profInput = formElement.querySelector('#user-profession');
+const cardsElement = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card').content;
+const cards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+cards.forEach(card => addCard(card.name, card.link));
+
+function addCard(name, link) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__photo').src = link;
+  cardElement.querySelector('.card__photo').alt = name;
+  cardElement.querySelector('.card__title').textContent = name;
+  cardsElement.append(cardElement);
+}
 
 function popupOpen() {
   nameInput.value = nameElement.textContent;
